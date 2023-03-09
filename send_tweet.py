a879f046f9104ee2478cfb772fd391f1ca0206aa
aitor <penguinft1@gmail.com>
2023-03-09 22:49:54 +0000

    loop fix

--- send_tweet.py
+++ send_tweet.py
@@ -30,47 +30,47 @@ mastodon = Mastodon(
 )
 
 
-
+while True:
+        #send message every hour
+        interval = 60
+        try:
 #ETH price, 24h change and Gas price
 
     # Get the start and end times of the current day in UTC
-today = datetime.now(timezone.utc).date()
-start_time = int(datetime(today.year, today.month, today.day, tzinfo=timezone.utc).timestamp())
-end_time = int((datetime(today.year, today.month, today.day, tzinfo=timezone.utc) + timedelta(days=1)).timestamp())    
+            today = datetime.now(timezone.utc).date()
+            start_time = int(datetime(today.year, today.month, today.day, tzinfo=timezone.utc).timestamp())
+            end_time = int((datetime(today.year, today.month, today.day, tzinfo=timezone.utc) + timedelta(days=1)).timestamp())    
 
     # Convert the current gas price to USD using the current ETH/USD price
-response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_24hr_change=true')
-data=response.json()
-eth_usd_price = data['ethereum']['usd']
-eth_usd_24h_change = data['ethereum']['usd_24h_change']
+            response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_24hr_change=true')
+            data=response.json()
+            eth_usd_price = data['ethereum']['usd']
+            eth_usd_24h_change = data['ethereum']['usd_24h_change']
 
     
         # Get the current gas price
-current_gas_price_gwei = w3.eth.gas_price / 10**9
-current_gas_price_usd = current_gas_price_gwei * 10**-9 * 21000 * eth_usd_price
+            current_gas_price_gwei = w3.eth.gas_price / 10**9
+            current_gas_price_usd = current_gas_price_gwei * 10**-9 * 21000 * eth_usd_price
 
 
 
 # Get historical gas prices for the last 24 hours grouped by hour
 
-average_gas_price_usd = []
-for i in range(24):
-        hour_ago = datetime.now(timezone.utc) - timedelta(hours=i+1)
-        start_time = int(hour_ago.timestamp())
-        end_time = int((hour_ago + timedelta(hours=1)).timestamp())
-        gas_prices = w3.eth.get_block('latest', full_transactions=False)['gasUsed']
-        total_gas_cost = gas_prices * w3.eth.gas_price
-        average_gas_price_usd.append(total_gas_cost / 1e18)
+            average_gas_price_usd = []
+            for i in range(24):
+                hour_ago = datetime.now(timezone.utc) - timedelta(hours=i+1)
+                start_time = int(hour_ago.timestamp())
+                end_time = int((hour_ago + timedelta(hours=1)).timestamp())
+                gas_prices = w3.eth.get_block('latest', full_transactions=False)['gasUsed']
+                total_gas_cost = gas_prices * w3.eth.gas_price
+            average_gas_price_usd.append(total_gas_cost / 1e18)
 
-average_gas_price_usd = sum(average_gas_price_usd) / len(average_gas_price_usd)
+            average_gas_price_usd = sum(average_gas_price_usd) / len(average_gas_price_usd)
 
 
 #Send out the tweet
 
-while True:
-        #send message every hour
-        interval = 60
-        try:
+
             tweet_text = f"🔷 ETH price is ${eth_usd_price} ({eth_usd_24h_change:+.2f}% last 24h)\n🔥 Current Gas price: ${current_gas_price_usd:.2f} || Avg Gas Price last 24h: ${average_gas_price_usd:.2f}\n#Ethereum #Gas"
         # Post to Mastodon
             mastodon.toot(tweet_text)
@@ -89,11 +89,12 @@ while True:
                 print('Error posting tweet via IFTTT:', response.text)
 
         except Exception as e:
-        
-            print('Error posting text:', e)
-
+            print('Error posting text:', e)        # Code to execute regardless of whether an exception was raised or not
+            print('Interval completed. Waiting for next interval...')
     # Wait for the interval before posting again
-        time.sleep(interval)
+
+        finally:
+            time.sleep(interval)
 
 
 
