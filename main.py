aa0acd48f91b79983ade1c11a7d5a1b0ae74f86e
aitor <penguinft1@gmail.com>
2023-03-11 17:22:30 +0000

    clean up

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('/path/to/mastodon/package')
-
 from mastodon import Mastodon
 from datetime import datetime, timezone, timedelta
 from web3 import Web3
@@ -10,17 +7,16 @@ from dotenv import load_dotenv
 import os
 
 
-#API KEYS and env variables
 
 def send_tweet(mega_send):
 
     load_dotenv()
 
+    #API KEYS and env variables
     alchemy = os.getenv('ALCHEMY_API_KEY')
     alchemy_api_key=os.getenv('ALCHEMY_KEY')
     etherscan_api_key = os.getenv('ETHERSCAN_API_KEY')
     ifttt=os.getenv('ifttt_key')
-    google_function_source = os.getenv('GOOGLE_FUNCTION_SOURCE')
 
 
 
@@ -38,28 +34,27 @@ def send_tweet(mega_send):
 
     while True:
             try:
-    #ETH price, 24h change and Gas price
 
-        # Get the start and end times of the current day in UTC
+                #ETH price, 24h change and Gas price
+
+                # Get the start and end times of the current day in UTC
                 today = datetime.now(timezone.utc).date()
                 start_time = int(datetime(today.year, today.month, today.day, tzinfo=timezone.utc).timestamp())
                 end_time = int((datetime(today.year, today.month, today.day, tzinfo=timezone.utc) + timedelta(days=1)).timestamp())    
 
-        # Convert the current gas price to USD using the current ETH/USD price
+                # Convert the current gas price to USD using the current ETH/USD price
                 response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_24hr_change=true')
                 data=response.json()
                 eth_usd_price = data['ethereum']['usd']
                 eth_usd_24h_change = data['ethereum']['usd_24h_change']
 
-
-            # Get the current gas price
+                # Get the current gas price
                 current_gas_price_gwei = w3.eth.gas_price / 10**9
                 current_gas_price_usd = current_gas_price_gwei * 10**-9 * 21000 * eth_usd_price
 
 
 
-    # Get historical gas prices for the last 24 hours grouped by hour
-
+                # Get historical gas prices for the last 24 hours grouped by hour
                 average_gas_price_usd = []
                 for i in range(24):
                     hour_ago = datetime.now(timezone.utc) - timedelta(hours=i+1)
@@ -71,32 +66,21 @@ def send_tweet(mega_send):
 
                 average_gas_price_usd = sum(average_gas_price_usd) / len(average_gas_price_usd)
 
-    # Get current block number and difficulty
-                block = w3.eth.get_block('latest')
-                block_number = block.number
-                difficulty = block.difficulty
-
-    # Calculate the block time in seconds
-                block_time = block.timestamp - w3.eth.get_block(block_number - 1).timestamp
-
-    # Calculate estimated network hashrate
-                hash_rate = 2 ** 32 * difficulty // block_time / 1000000
-
-                print(f"Estimated network hashrate: {hash_rate:.2f} MH/s")
 
 
-    #Send out the tweet
+                #Send out the tweet
+                tweet_text = f"🛰️ Reporting Live from #Ethereum Mainnet Station\n\n🔷 #ETH price is ${eth_usd_price} ({eth_usd_24h_change:+.2f}% last 24h)\n🔥 Gas price: ${current_gas_price_usd:.2f} || Avg Gas Price last 24h: ${average_gas_price_usd:.2f}\n#DeFi #NFT #BTC #Crypto"
+                
 
 
-                tweet_text = f"🛰️ Reporting Live from #Ethereum Mainnet Station\n\n🔷 #ETH price is ${eth_usd_price} ({eth_usd_24h_change:+.2f}% last 24h)\n🔥 Gas price: ${current_gas_price_usd:.2f} || Avg Gas Price last 24h: ${average_gas_price_usd:.2f}\n💻 Network Hash Rate: {hash_rate / 10**9:.2f} MH/s"
-            # Post to Mastodon
+                # Post to Mastodon
                 mastodon.toot(tweet_text)
 
-            # Post to Steemit
+                # Post to Steemit
                 #c = Comment("", steem_instance=steem)
                 #c.post(text)
 
-            # Post to IFTTT
+                # Post to IFTTT
                 payload = {'tweet_text': tweet_text}
                 response = requests.post(ifttt_url, json=payload)
 
@@ -111,7 +95,8 @@ def send_tweet(mega_send):
                     print('Error posting tweet via IFTTT:', response.text)
 
             except Exception as e:
-                print('Error posting text:', e)        # Code to execute regardless of whether an exception was raised or not
+                print('Error posting text:', e)        
+                
 
 def main(request):
     mega_send = request.args.get('mega_send', default='')
