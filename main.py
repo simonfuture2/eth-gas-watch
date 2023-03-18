f518189552fa856b0ca71b053a7efd06b2c4ed5b
aitor <penguinft1@gmail.com>
2023-03-18 11:29:22 +0100

    add daily f&g, refactor, rm tx count update msg 4h

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ async def send_message_async(channel_id, tweet_text):
     bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
     await bot.send_message(chat_id=channel_id, text=tweet_text)
 
-
-async def send_hourly_messages():
+#Send status update every 4h
+async def send_four_hourly_messages():
     while True:
         utc_time = datetime.utcnow()
         formatted_time = utc_time.strftime('%m-%d-%Y %I:%M %p')
@@ -41,35 +41,29 @@ async def send_hourly_messages():
         current_gas_price_gwei = w3.eth.gas_price / 10**9
         current_gas_price_usd = current_gas_price_gwei * 10**-9 * 21000 * eth_usd_price
 
-        latest_block = w3.eth.block_number
-        one_hour_ago_block = w3.eth.get_block(latest_block - (60 * 60 // 13))
-
-        tx_count = 0
-        for i in range(one_hour_ago_block.number, latest_block + 1):
-            tx_count += w3.eth.get_block_transaction_count(i)
-
-        tweet_text = f"{formatted_time} UTC Live from #Ethereum Mainnet\n\n• $ETH price is ${eth_usd_price} ({eth_usd_1h_change:+.2f}% 1h)\n• Market Cap: ${market_cap:,}\n• Dominance: {eth_dominance:.2f}% | {diff:.2f} behind $BTC\n• Gas price: {current_gas_price_gwei:.2f} GWEI = ${current_gas_price_usd:.2f}\n• 1h transactions: {tx_count}\n\n#DeFi #NFT #Crypto"
+        tweet_text = f"{formatted_time} UTC Live from #Ethereum Mainnet\n\n• $ETH price is ${eth_usd_price} ({eth_usd_1h_change:+.2f}% 1h)\n• Market Cap: ${market_cap:,}\n• Dominance: {eth_dominance:.2f}% | {diff:.2f} behind $BTC\n• Gas price: {current_gas_price_gwei:.2f} GWEI = ${current_gas_price_usd:.2f}\n\n#DeFi #NFT #Crypto"
 
         print(tweet_text)
 
         await send_message_async(channel_id, tweet_text)
-        await asyncio.sleep(3600)
+        await asyncio.sleep(14400)
 
+
+def ordinal(n):
+    return str(n) + ('th' if 4 <= n % 100 <= 20 else {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th'))
 
 async def send_daily_message():
     while True:
         now = datetime.utcnow()
-        
         scheduled_time = time(hour=9, minute=0, second=0, microsecond=0)
-
-        now_datetime = datetime.combine(now.date(), scheduled_time)
         
+        now_datetime = datetime.combine(now.date(), now.time())
+        next_schedule_datetime = datetime.combine(now.date(), scheduled_time)
+
         if now.time() > scheduled_time:
-            now_datetime += timedelta(days=1)
+            next_schedule_datetime += timedelta(days=1)
 
-        scheduled_time = (now_datetime + timedelta(days=1)).time()
-        
-        time_diff = datetime.combine(now.date(), scheduled_time) - now
+        time_diff = next_schedule_datetime - now_datetime
         
         await asyncio.sleep(time_diff.total_seconds())
         
@@ -78,20 +72,19 @@ async def send_daily_message():
         data = response.json()
         fear_greed_value = data["data"][0]["value"]
         sentiment = data["data"][0]["value_classification"]
-        date = datetime.utcfromtimestamp(int(data["data"][0]["timestamp"])).strftime('%B %d' + ('th' if 4 <= int(data["data"][0]["timestamp"][8:10]) % 100 <= 20 else {1:'st', 2:'nd', 3:'rd'}.get(int(data["data"][0]["timestamp"][8:10]) % 10, 'th')))
+        date = datetime.utcfromtimestamp(int(data["data"][0]["timestamp"])).strftime('%B ') + ordinal(int(data["data"][0]["timestamp"][8:10]))
         
-        message = f"GM #CryptoTwitter\n\n{date} Crypto Fear and Greed Index: {fear_greed_value}\n\nSentiment: {sentiment}/100"
+        message = f"GM #CryptoTwitter\n\n{date} Crypto Fear and Greed Index stands at {fear_greed_value}/100\n\nSentiment: {sentiment}"
         
         print(message)
 
         await send_message_async(channel_id, message)
 
-        break
 
 
 async def main():
     tasks = [
-        asyncio.create_task(send_hourly_messages()),
+        asyncio.create_task(send_four_hourly_messages()),
         asyncio.create_task(send_daily_message()),
     ]
     await asyncio.gather(*tasks)
